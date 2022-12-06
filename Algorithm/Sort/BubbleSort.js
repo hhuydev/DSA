b942ebdf1c2cf25591a0dffab4d1ba09b51a2d01
hhuydev <hahuy.dev@gmail.com>
2022-12-06 16:49:41 +0700

    algorithm: bubble-sort

---
+++ Algorithm/Sort/BubbleSort.js
@@ -0,0 +1,19 @@
+let arr = [29, 3, -1, 0, 33, 122, -44, 3.5, 2];
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  console.log(array);
+};
+
+(() => {
+  console.log("Before array is not sorted: ", arr);
+  console.log("After array is sorted: " + bubbleSort(arr));
+})();
